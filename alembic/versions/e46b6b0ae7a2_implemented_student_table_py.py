eecfe1209b37441ce74f33064225655ae7acd691
Krzysiek <krzysztofbytnar@gmail.com>
2022-11-20 11:55:57 +0100

    Updated student table

---
+++ alembic/versions/e46b6b0ae7a2_implemented_student_table_py.py
@@ -0,0 +1,14 @@
+
+# revision identifiers, used by Alembic.
+revision = 'e46b6b0ae7a2'
+down_revision = '7ab212966df2'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
